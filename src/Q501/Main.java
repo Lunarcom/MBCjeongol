c18a51e624fd721899c964d007fc2277cff1985f
Moonbo <uoowoq@gmail.com>
2026-06-10 18:21:41 +0900

    501

--- src/Q501/Main.java
+++ src/Q501/Main.java
@@ -2,6 +2,6 @@ package Q501;
 
 public class Main {
     public static void main(string[] args) {
-        System.out.println("Fun Programming!");
+        System.out.println("두번째 프로그램입니다. 한줄에 프린트합니다".);
     }
 }
